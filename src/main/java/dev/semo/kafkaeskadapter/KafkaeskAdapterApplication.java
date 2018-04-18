b71e6ebd9e3570f321b056c01fad16e208c7c15b
semo <semox78@gmail.com>
2018-04-18 09:45:52 +0200

    * Added basic GET method to have a skeleton of further Request-Methods

--- src/main/java/dev/semo/kafkaeskadapter/KafkaeskAdapterApplication.java
+++ src/main/java/dev/semo/kafkaeskadapter/KafkaeskAdapterApplication.java
@@ -5,8 +5,9 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
-
+@ComponentScan("dev.semo.kafkaeskadapter.controllers")
 @SpringBootApplication
 public class KafkaeskAdapterApplication {
 
--- src/main/java/dev/semo/kafkaeskadapter/controllers/NumberPlateController.java
+++ src/main/java/dev/semo/kafkaeskadapter/controllers/NumberPlateController.java
@@ -1,7 +1,19 @@
 package dev.semo.kafkaeskadapter.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class NumberPlateController {
 
-
+    @RequestMapping(value = "/data", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity getSimpleOK() {
+        return new ResponseEntity(HttpStatus.ACCEPTED);
+    }
 
 }
